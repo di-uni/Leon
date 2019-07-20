0957924138648505abdbbd2e0e1ee34221b6a515
vano <vano1205@naver.com>
2019-07-20 22:41:37 +0900

    commit2

---
+++ Assets/Scripts/ObjectColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColor : MonoBehaviour
+{
+    void OnSetColor(Color color)
+	{
+		Material mt = new Material(GetComponent<Renderer>().sharedMaterial);
+		mt.color = color;
+		GetComponent<Renderer>().material = mt;
+	}
+
+	void OnGetColor(ColorPicker picker)
+	{
+		picker.NotifyColor(GetComponent<Renderer>().material.color);
+	}
+}
